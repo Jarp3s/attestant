d6cdabd1473005cee1bfb4d06240c3607867f1bd
Jasper Hofman <j.d.hofman@students.uu.nl>
2023-02-26 16:57:49 +0100

    Add basic classes, setup atomic program-flow

---
+++ attestant/DepthFirstTransformation.cs
@@ -0,0 +1,24 @@
+﻿using attestant.IPA;
+
+namespace attestant;
+
+
+/// <summary>
+///    Algorithm that performs dfs by applying layers of sound laws on a given word;
+///    this way the given word is transformed into other phoneme-arrangements
+/// </summary>
+
+public class DepthFirstTransformation
+{
+    private readonly List<HashSet<SoundLaw>> _soundLaws;
+
+    public DepthFirstTransformation(List<HashSet<SoundLaw>> soundLaws)
+    {
+        _soundLaws = soundLaws;
+    }
+
+    public void TransformWord(Word word)
+    {
+        
+    }
+}
--- attestant/Program.cs
+++ attestant/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using attestant;
+using attestant.IPA;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+List<HashSet<SoundLaw>> soundLaws = new();
+List<Word> words = new();
+
+var dft = new DepthFirstTransformation(soundLaws);
+foreach (var word in words)
+    dft.TransformWord(word);
+    
