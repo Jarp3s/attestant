3c9c6757f20a1a7a46366013d90eef445bb25fa8
Jasper Hofman <j.d.hofman@students.uu.nl>
2023-03-03 11:14:08 +0100

    Refactor/delete SoundLaw classes

--- attestant/DepthFirstTransformation.cs
+++ attestant/DepthFirstTransformation.cs
@@ -1,44 +1,30 @@
 ﻿using attestant.DataStructures;
-using attestant.SoundLaws;
 
 namespace attestant;
 
 
 /// <summary>
-///    Algorithm that performs dfs by applying layers of sound laws on a given word;
+///    Algorithm that performs dfs by a layer of sound laws on a given word;
 ///    this way the given word is transformed into other phoneme-arrangements
 /// </summary>
 
 public class DepthFirstTransformation
 {
-    private readonly List<HashSet<SoundLaw>> _soundLaws;
+    private readonly List<SoundLaw> _soundLaws;
 
-    public DepthFirstTransformation(List<HashSet<SoundLaw>> soundLaws)
+    public DepthFirstTransformation(List<SoundLaw> soundLaws)
     {
         _soundLaws = soundLaws;
     }
 
-    public HashSet<UNode<Word>> TransformWord(Word word)
+    public HashSet<UNode<string>> TransformWord(string word)
     {
-        UNode<Word> originalWordNode = new (word);
-        return ApplyLawLayer(new HashSet<UNode<Word>>(), originalWordNode, 0);
+        UNode<string> originalWordNode = new (word);
+        return ApplyLawLayer(new HashSet<UNode<string>>(), originalWordNode, 0);
     }
 
-    private HashSet<UNode<Word>> ApplyLawLayer(HashSet<UNode<Word>> reconstructions, UNode<Word> wordNode, int layer)
+    private HashSet<UNode<string>> ApplyLawLayer(HashSet<UNode<string>> reconstructions, UNode<string> wordNode, int layer)
     {
-        if (layer == _soundLaws.Count) // Base case
-        {
-            reconstructions.Add(wordNode);
-            return reconstructions;
-        }
-        
-        Word curWord = wordNode.Value;
-        Word newWord;
-        
-        foreach (var soundLaw in _soundLaws[layer]) // Recursive case 1
-            if (!(newWord = soundLaw.ApplyOnWord(curWord)).SequenceEqual(curWord))
-                ApplyLawLayer(reconstructions, wordNode.AddDescendant(newWord), ++layer);
-
-        return ApplyLawLayer(reconstructions, wordNode, ++layer); // Recursive case 2
+        throw new NotImplementedException();
     }
 }
---
+++ attestant/SoundLaw.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace attestant;
+
+
+public class SoundLaw
+{
+    private readonly Regex _antecedent;
+    private readonly HashSet<string> _consequents;
+
+    public SoundLaw(string antecedent, HashSet<string> consequents)
+    {
+        _antecedent = new Regex(antecedent);
+        _consequents = consequents;
+    }
+
+    public static HashSet<SoundLaw> Parse(string inputLaw)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> Apply(string word)
+    {
+        return _consequents.Select(consequent => _antecedent.Replace(word, consequent));
+    }
+}
