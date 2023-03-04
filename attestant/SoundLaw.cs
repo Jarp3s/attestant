440938adf781bd06704696e18068d70a9ce05ab8
Jasper Hofman <j.d.hofman@students.uu.nl>
2023-03-04 16:56:10 +0100

    Work on sound law parsing

--- attestant/SoundLaw.cs
+++ attestant/SoundLaw.cs
@@ -26,19 +26,40 @@ public class SoundLaw
     public static SoundLaw Parse(string inputLaw)
     { 
         var law = Regex.Replace(inputLaw, @"[\s*]", @"");
-        var segments = Regex.Split(law, @"[+>/]");
-        var sound = segments[2];
+        var lawSegments = Regex.Split(law, @"[+>/]");
+        var sound = lawSegments[2];
 
         /*
          * replace all cover symbols w/ [lookup(cover)]
          * replace all other special symbols
          * replace symbol between '()' w/ ? 
          * replace '#' at start w/ ^ & at end w/ $
-         * _antecedent  = replace '_' w/ ([symbols])
-         * _consequents = foreach symbol, replace _ w/ symbol in a way of substitution.
-         */ 
+         */
         
-        throw new NotImplementedException();
+        // Create a RegEx char-group using by processing all symbols in the antecedent
+        var antecedentSymbols = Regex.Split(lawSegments[0], @",");
+        var replacedSymbols = "[";
+        foreach (var symbol in antecedentSymbols)
+        {
+            // TODO: convert special symbols to RegEx
+            replacedSymbols += symbol;
+        }
+        replacedSymbols += "]";
+
+        // Create a RegEx pattern-string (antecedent) by grouping the 3 segments of the antecedent
+        var soundSegments = Regex.Split(sound, @"_");
+        var antecedent = $"({soundSegments[0]})({replacedSymbols})({soundSegments[1]})";
+        
+        // Create RegEx replacement-strings (consequents) by processing each symbol individually
+        var consequentSymbols = Regex.Split(lawSegments[1], @",");
+        HashSet<string> consequents = new();
+        foreach (var symbol in consequentSymbols)
+        {
+            // TODO: convert special symbols to RegEx
+            consequents.Add($"$1{symbol}$3");
+        }
+
+        return new SoundLaw(antecedent, consequents, inputLaw);
     }
 
     /// <summary>
