5923e76ccbe62d211ec3ae0a00a9c3d77ff9a58e
Jasper Hofman <j.d.hofman@students.uu.nl>
2023-03-04 17:01:55 +0100

    Clean up Program entry point

--- attestant/Program.cs
+++ attestant/Program.cs
@@ -5,18 +5,8 @@ using System.Text.RegularExpressions;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 
-var law = Regex.Replace(Console.ReadLine()!, @"[\s*]", @"");
-var segments = Regex.Split(law, @"[+>/]");
-foreach (var seg in segments)
-    Console.WriteLine(seg);
-
-/*var result = Regex.Replace(Console.ReadLine()!, @"(.*)(_)(.*)", "$1x$3");
-Console.WriteLine(result);*/
-
-/*
 List<SoundLaw> soundLaws = new();
 const string word = "";
 
 var dft = new DepthFirstTransformation(soundLaws);
 HashSet<UNode<string>> reconstructions = dft.TransformWord(word);
-*/
