64b23c699f8f77db9b9a2cfc335ec3ade04c569d
Jasper Hofman <j.d.hofman@students.uu.nl>
2023-03-05 21:32:49 +0100

    Work on reading sound laws from JSON-files

--- attestant/InputReaders/SoundLawReader.cs
+++ attestant/InputReaders/SoundLawReader.cs
@@ -1,7 +1,38 @@
+using System.Text.Json;
+
 namespace attestant.InputReaders;
 
 
-public class SoundLawReader
+public static class SoundLawReader
 {
-    
+    public static List<LanguageDevelopment> LanguageDevelopments
+    {
+        get
+        {
+            List<LanguageDevelopment> languageDevelopments = new();
+
+            var fileName = "";
+            var jsonString = File.ReadAllText(fileName);
+            languageDevelopments.Add(JsonSerializer.Deserialize<LanguageDevelopment>(jsonString)!);
+            
+            fileName = "";
+            jsonString = File.ReadAllText(fileName);
+            languageDevelopments.Add(JsonSerializer.Deserialize<LanguageDevelopment>(jsonString)!);
+            
+            return languageDevelopments;
+        }
+    }
+}
+
+public class LanguageDevelopment
+{
+    public string Language { get; } = null!;
+    public SoundLaw[] SoundLaws { get; } = null!;
+
+    public class SoundLaw
+    {
+        public string Number { get; } = null!;
+        public string Law { get; } = null!;
+        public string Context { get; } = null!;
+    }
 }
