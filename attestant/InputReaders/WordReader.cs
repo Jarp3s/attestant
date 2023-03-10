8288a6d71683f15ffddbc8831aa27d65e31c949f
PeculiarStitch <rozendaal.thomas@gmail.com>
2023-03-10 14:00:48 +0100

    (Backup of work on writing to sound)
    (Backup of work on writing to sound)

--- attestant/InputReaders/WordReader.cs
+++ attestant/InputReaders/WordReader.cs
@@ -1,10 +1,18 @@
+using attestant.Utilities;
+
 namespace attestant.InputReaders;
 
 
 public class WordReader
 {
+    Spelling WelshSpelling;
+    Spelling IrishSpelling;
+
     internal List<WordSet> ReadWords()
     {
+        WelshSpelling = new();
+        IrishSpelling = new();
+
         string fileName = "cognates.txt";
         string filePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "Resources", fileName));
         FileStream s = new(filePath, FileMode.Open);
@@ -26,6 +34,16 @@ public class WordReader
     {
         string[] split = cognates.Split(';');
         WordSet word = new(split[1].TrimStart(), split[2].TrimStart(), split[0]);
+        word.ProtoCeltic.Replace('y', 'j');
         return word;
+    }
+
+    private WordSet Phonetic(WordSet words)
+    {
+        // Proto-Celtic forms are fine already
+        words.OldIrish = IrishSpelling.Phonetic(words.OldIrish);
+        words.MiddleWelsh = IrishSpelling.Phonetic(words.MiddleWelsh);
+
+        return words;
     }
 }
---
+++ attestant/Utilities/Spelling.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attestant.Utilities
+{
+    internal class Spelling
+    {
+        public Spelling()
+        {
+
+        }
+
+        internal string Phonetic(string written)
+        {
+            return "";
+        }
+    }
+}
